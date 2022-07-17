ff6252f3e9304d99d3f0bdcf45c3a96c6b140274
Max Engelke <max.engelke1989@gmail.com>
2022-07-17 12:33:04 +0200

    added features to install_server.sh

--- install_server.sh
+++ install_server.sh
@@ -15,6 +15,8 @@ if [ -z "$1" ]; then
         INSTALL_DIR=$1
 fi
 
+INSTALL_DIR="$(realpath "${INSTALL_DIR}")"
+
 echo "Install to $INSTALL_DIR"
 
 if [[ ! -d "$INSTALL_DIR" ]]; then
@@ -25,8 +27,10 @@ fi
 cp "$CURRENT_DIR/server/DynDnsServer.py" "$INSTALL_DIR"
 cp -r "$CURRENT_DIR/dyndns_utils" "$INSTALL_DIR"
 
-#install socat
+#install dependencies
+apt-get update
 apt-get install socat
+apt-get install pip
 
 #incstall crypto for py
 echo "Install python module cryptography"
@@ -43,4 +47,36 @@ echo "Creating server config file $CONFIG_FILE"
 echo "server_port=
 private_key=$INSTALL_DIR/private_key.pem" > $CONFIG_FILE
 
-#add daemon
+#create service
+SCRIPT_FILE="$INSTALL_DIR/DynDnsServer.py"
+chmod +x "$SCRIPT_FILE"
+
+SERVER_USER="dyndnsserveruser"
+#create user for service
+useradd -r -s /bin/false $SERVER_USER
+chown -R py $SERVER_USER:$SERVER_USER "$INSTALL_DIR"
+
+#unit file
+
+echo "[Unit]
+Description=DynDnsServer
+After=syslog.target
+
+[Service]
+Type=simple
+User=$SERVER_USER
+Group=$SERVER_USER
+WorkingDirectory=$INSTALL_DIR
+ExecStart=$SCRIPT_FILE
+SyslogIdentifier=DynDnsServer
+StandardOutput=syslog
+StandardError=syslog
+Restart=always
+RestartSec=3
+
+[Install]
+WantedBy=multi-user.target" > "/etc/systemd/system/DynDnsServer.service"
+
+systemctl enable DynDnsServer
+systemctl start DynDnsServer
+

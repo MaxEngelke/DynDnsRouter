4606d9607c3332b8a273160c30c36b0f3d7daa29
Max Engelke <max.engelke1989@gmail.com>
2022-07-31 12:24:49 +0200

    finished installation files

--- install_client.sh
+++ install_client.sh
@@ -36,10 +36,48 @@ client=
 tunnel_ports_tcp=
 tunnel_ports_udp=
 current_ip=
+current_ip_version=ipv6
 public_key=" > "$CONFIG_FILE"
+
+#install dependencies
+apt-get update
+apt-get install pip
 
 #incstall crypto for py
 echo "Install python module cryptography"
 pip3 install cryptography
 
+#add user
+
+CLIENT_USER="dyndnsclientuser"
+#create user for service
+useradd -r -s /bin/false $SERVER_USER
+chown -R $SERVER_USER:$SERVER_USER "$INSTALL_DIR"
+
 #install cron job
+
+COMMAND="python $INSTALL_DIR/DynDnsClientUpdate.py"
+
+grep -qi "$CLIENT_USER" /etc/cron.allow
+
+if [ $? != 0 ]; then
+    echo "$CLIENT_USER" >> /etc/cron.allow
+fi
+
+(crontab -u $CLIENT_USER -l ; echo "0 * * * * $COMMAND") | crontab -u $CLIENT_USER -
+
+#create uninstall_client.sh
+
+if [ -f uninstall_client.sh ]; then
+    rm uninstall_client.sh
+fi
+
+echo "#!/bin/bash
+
+rm -rf $INSTALL_DIR
+
+crontab -u $CLIENT_USER -l | grep -v '$COMMAND'  | crontab -u $CLIENT_USER -
+
+userdel $SERVER_USER
+
+" >> "uninstall_client.sh"
--- install_server.sh
+++ install_server.sh
@@ -17,6 +17,13 @@ fi
 
 INSTALL_DIR="$(realpath "${INSTALL_DIR}")"
 
+#Read port for server socket
+read -p "Enter server port(50110) : " PORT
+
+if [ -z "$PORT" ]; then
+    PORT=50110
+fi
+
 echo "Install to $INSTALL_DIR"
 
 if [[ ! -d "$INSTALL_DIR" ]]; then
@@ -28,13 +35,15 @@ cp "$CURRENT_DIR/server/DynDnsServer.py" "$INSTALL_DIR"
 cp -r "$CURRENT_DIR/dyndns_utils" "$INSTALL_DIR"
 
 #install dependencies
+echo "Install linux packages"
 apt-get update
 apt-get install socat
 apt-get install pip
 
 #incstall crypto for py
-echo "Install python module cryptography"
+echo "Install python modules"
 pip3 install cryptography
+pip3 install psutils
 
 #create keys
 python "$INSTALL_DIR/dyndns_utils/crypto_utils.py" "$INSTALL_DIR"
@@ -44,7 +53,7 @@ CONFIG_FILE="$INSTALL_DIR/server.cfg"
 
 echo "Creating server config file $CONFIG_FILE"
 
-echo "server_port=
+echo "server_port=$PORT
 private_key=$INSTALL_DIR/private_key.pem" > $CONFIG_FILE
 
 #create service
@@ -54,7 +63,7 @@ chmod +x "$SCRIPT_FILE"
 SERVER_USER="dyndnsserveruser"
 #create user for service
 useradd -r -s /bin/false $SERVER_USER
-chown -R py $SERVER_USER:$SERVER_USER "$INSTALL_DIR"
+chown -R $SERVER_USER:$SERVER_USER "$INSTALL_DIR"
 
 #unit file
 
@@ -80,3 +89,41 @@ WantedBy=multi-user.target" > "/etc/systemd/system/DynDnsServer.service"
 systemctl enable DynDnsServer
 systemctl start DynDnsServer
 
+#try open port in firewalld
+
+which firewall-cmd | grep -o firewall-cmd > /dev/null
+
+if [ $? -eq 0 ]; then
+    firewall-cmd --zone=public --add-port=$PORT/udp --permanent
+    firewall-cmd --reload
+fi
+
+#create uninstall_server.sh
+
+if [ -f uninstall_server.sh ]; then
+    rm uninstall_server.sh
+fi
+
+echo "#!/bin/bash
+
+systemctl stop DynDnsServer.service
+systemctl disable DynDnsServer.service
+rm -f /etc/systemd/system/DynDnsServer.service
+rm -f /etc/systemd/system/DynDnsServer.service # and symlinks that might be related
+rm -f /usr/lib/systemd/system/DynDnsServer.service
+rm -f /usr/lib/systemd/system/DynDnsServer.service # and symlinks that might be related
+systemctl daemon-reload
+systemctl reset-failed
+
+rm -rf $INSTALL_DIR
+
+userdel $SERVER_USER
+
+which firewall-cmd | grep -o firewall-cmd > /dev/null
+
+if [ \$? -eq 0 ]; then
+    firewall-cmd --zone=public --remove-port=$PORT/udp --permanent
+    firewall-cmd --reload
+fi
+
+" >> "uninstall_server.sh"
